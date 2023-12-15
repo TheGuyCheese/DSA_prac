533c961e6bb56cea15032dcdec98ad495b464977
TheGuyCheese <s.aditya.digi@gmail.com>
2023-12-15 20:44:04 +0530

    pattern in characters

---
+++ Patterns/charac_pattern.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+int main()
+{
+    int n=5;
+    //outerloop
+    for(int i=0;i<n;i++){
+        //innerloop
+        for(char ch='A';ch<='A'+i;ch++){
+            std::cout<<ch;
+        }
+        std::cout<<"\n";
+    }
+    return 0;
+}
