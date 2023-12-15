53083d8ce4e72c999365781a4db9d693b3985785
TheGuyCheese <s.aditya.digi@gmail.com>
2023-12-15 20:44:47 +0530

    some more patterns

---
+++ Patterns/rev_char_pattern.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+int main()
+{
+    int n=5;
+    //outerloop
+    for(int i=n;i>=1;i--){
+        //innerloop
+        for(char ch='A';ch<='A'+i-1;ch++){
+            std::cout<<ch;
+        }
+        std::cout<<"\n";
+    }
+    return 0;
+}
