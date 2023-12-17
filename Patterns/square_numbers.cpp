fef82a255bf97d983835bbc3ec63b2c4ffef88cb
TheGuyCheese <s.aditya.digi@gmail.com>
2023-12-17 15:10:42 +0530

    another number matrix

---
+++ Patterns/square_numbers.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+int main()
+{
+    int n=4;
+    //outerloop
+    for(int i=0;i<2*n-1;i++){
+        //innerloop
+        for(int j=0;j<2*n-1;j++){
+            int top = i;
+            int left = j;
+            int right = (2*n-2)-j;
+            int bottom = (2*n-2)-i;
+            std::cout<<(n-std::min(std::min(top, bottom), std::min(left, right)));
+        }
+        std::cout<<"\n";
+    }
+    return 0;
+}
