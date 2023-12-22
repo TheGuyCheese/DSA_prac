d4bf65ddfc9033bafda27f641bbd6ee7fb45501d
TheGuyCheese <s.aditya.digi@gmail.com>
2023-12-22 21:09:22 +0530

    N to 1 recursion, No global Variable

---
+++ Recursion/N_to_1_recursion.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+void solution(int n){
+    if(n<1) return;
+    std::cout<<n<<" ";
+    solution(n-1);
+}
+int main()
+{
+    int n;
+    std::cin>>n;
+    solution(n);
+    return 0;
+}
